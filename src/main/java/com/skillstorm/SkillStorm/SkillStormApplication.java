eb87b53a002ea9798865cc64b91f01b52c5f13e6
Tyler Katsha <tylerkatsha14@gmail.com>
2026-07-29 16:18:21 +0200

    Initial Commit

---
+++ src/main/java/com/skillstorm/SkillStorm/SkillStormApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.SkillStorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillStormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillStormApplication.class, args);
+	}
+
+}
