edceb477792e8abbe3be75b956ab22e29fca02d7
Mateo Joubert <mateoj@tutamail.com>
2026-08-08 20:20:45 +0200

    Lowercased artifactId
    artifactId should only contain lower case letters according to [Maven naming conventions](https://maven.apache.org/guides/mini/guide-naming-conventions.html#:~:text=lowercase).

--- src/main/java/com/skillstorm/skillstorm/SkillStormApplication.java
+++ src/main/java/com/skillstorm/skillstorm/SkillStormApplication.java
@@ -1,4 +1,4 @@
-package com.skillstorm.SkillStorm;
+package com.skillstorm.skillstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
