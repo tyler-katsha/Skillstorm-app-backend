49c66c5744d5922115ba66207278497cdf657319
Mateo Joubert <mateoj@tutamail.com>
2026-08-16 13:12:35 +0200

    Removed Leaderboard classes
    It doesn't make sense to have a separate entity table or set of classes for the Leaderboard. Instead, player rankings can be obtained by join queries on the User and Attempt tables.

--- src/main/java/com/skillstorm/skillstorm/repository/LeaderboardRepository.java
+++
@@ -1,6 +0,0 @@
-package com.skillstorm.skillstorm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.skillstorm.skillstorm.model.Leaderboard;
-
-public interface LeaderboardRepository extends JpaRepository<Leaderboard, Integer> {}
